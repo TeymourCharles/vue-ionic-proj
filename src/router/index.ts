ab870761f7559047707d5ce24e177697c3dbe70a
TeymourCharles <villanuevateymour27@gmail.com>
2024-09-15 02:19:33 +0800

    Modifies codes

--- src/router/index.ts
+++ src/router/index.ts
@@ -39,20 +39,6 @@ const routes: Array<RouteRecordRaw> = [
         path: 'loyaltypoints',
         component: () => import('@/views/LoyaltyPoints.vue')
       },
-
-
-      {
-        path: 'tab1',
-        component: () => import('@/views/Tab1Page.vue')
-      },
-      {
-        path: 'tab2',
-        component: () => import('@/views/Tab2Page.vue')
-      },
-      {
-        path: 'tab3',
-        component: () => import('@/views/Tab3Page.vue')
-      }
     ]
   }
 ]
